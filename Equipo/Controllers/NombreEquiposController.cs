f046da06c79b100f24dc5ca3f2ef934cd2e2e053
SeleneHdz01 <selene.hdz.f@gmail.com>
2022-08-21 23:06:33 -0500

    Se agrega proyeto Equipo

---
+++ Equipo/Controllers/NombreEquiposController.cs
@@ -0,0 +1,22 @@
+﻿using Equipo.Models;
+using Microsoft.AspNetCore.Mvc;
+namespace Equipo.Controllers
+{
+    public class NombreEquiposController :Controller
+    {
+        public IActionResult Crear()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Crear(NombreEquipos nombreEquipos)
+            {
+            if (!ModelState.IsValid)
+            {
+                return View(nombreEquipos);
+            }
+            return View();
+        }
+    }
+}
+
---
+++ Equipo/Models/NombreEquipo.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace Equipo.Models
+{
+    public class NombreEquipos
+    {
+       
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El campo Nombre es requerido")]
+        [StringLength(maximumLength:15, MinimumLength=6, ErrorMessage ="El nombre debe ser minimo de {2} y máximo de {1} caracteres")]
+        public string NombreEquipo { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        public DateTime FechaCreacion { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        public string Logo { get; set; }
+    
+    }
+}
+
